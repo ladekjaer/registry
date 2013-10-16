4807ef9868d954bbf979bbe5233e2f3de6b8bb44
Rasmus Ladekjær Pedersen <ladekjaer@gmail.com>
2013-10-17 00:49:17 +0200

    In case a system have multiple adresses the scripts sends just one.

--- client.sh
+++ client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n 1)
 curl -fsm 30 "http://registry.ladekjaer.org/push?ip=$ip&hostname=$(hostname)"
